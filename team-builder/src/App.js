600fa5d45e6aa55aa4d43a4a12b0e5ff56f543bb
reesharper <reestimharper@gmail.com>
2020-10-14 03:41:21 -0400

    completed MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Teammate from "./Teammate";
+import TeamForm from "./Form";
+import axios from "./axios";
 
-function App() {
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  role: "",
+};
+
+export default function App() {
+  const [teammates, setTeammates] = useState([]);
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue,
+    });
+  };
+
+  const submitForm = () => {
+
+    let newTeammate = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+
+    if (!newTeammate.username || !newTeammate.email || !newTeammate.role) return;
+    axios
+      .post("fakeapi.com", newTeammate)
+      .then((res) => {
+        setTeammates([...teammates, res.data]);
+        setFormValues(initialFormValues);
+      })
+      .catch((err) => {
+        // debugger;
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    axios.get("fakeapi.com").then((res) => setTeammates(res.data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Team List</h1>
+
+      <TeamForm
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+
+      {teammates.map((teammate) => {
+        return <Teammate key={teammate.id} details={teammate} />;
+      })}
     </div>
   );
 }
-
-export default App;
